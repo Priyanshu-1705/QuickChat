429b4023ac9ce67432c2fd6760f417c568a6c780
priyanshu <priyanshugangwar17@gmail.com>
2026-05-16 19:12:17 +0530

    Fix changes for Vercel.

--- server/server.js
+++ server/server.js
@@ -58,3 +58,7 @@ if (process.env.NODE_ENV !== "production") {
     const PORT = process.env.PORT || 5000;
     server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 }
+
+
+// Export for vercel
+export default server;
